bdf4f8deaed59cde998c03b3fadd42cd26e4c888
Alexander Paul Stevens <75080516+AlexanderPaulStevens@users.noreply.github.com>
2023-04-04 09:53:20 +0200

    This file originates from irhete
    https://github.com/irhete/predictive-monitoring-benchmark

--- EncoderFactory.py
+++ EncoderFactory.py
@@ -1,3 +1,6 @@
+# The code and architecture in this file stems from: hhttps://github.com/irhete/predictive-monitoring-benchmark
+# I have only added this file to my own GitHub, to make my own code reproducible.
+# I thank the authors for their valuable work.
 from encoders.AggregateTransformer import AggregateTransformer
         
 def get_encoder(method, case_id_col=None, static_cat_cols=None, static_num_cols=None, dynamic_cat_cols=None,
@@ -7,17 +10,9 @@ def get_encoder(method, case_id_col=None, static_cat_cols=None, static_num_cols=
     if method == "static":
         return StaticTransformer(case_id_col=case_id_col, cat_cols=static_cat_cols, num_cols=static_num_cols, fillna=fillna)
 
-    elif method == "last":
-        return LastStateTransformer(case_id_col=case_id_col, cat_cols=dynamic_cat_cols, num_cols=dynamic_num_cols, fillna=fillna)
-
     elif method == "agg":
         return AggregateTransformer(case_id_col=case_id_col, cat_cols=dynamic_cat_cols, num_cols=dynamic_num_cols, boolean=False, fillna=fillna)
 
-    elif method == "bool":
-        return AggregateTransformer(case_id_col=case_id_col, cat_cols=dynamic_cat_cols, num_cols=dynamic_num_cols, boolean=True, fillna=fillna)
-    
-    elif method == "index":
-        return IndexBasedTransformer(case_id_col=case_id_col, cat_cols=dynamic_cat_cols, num_cols=dynamic_num_cols, max_events=max_events, fillna=fillna)
 
     else:
         print("Invalid encoder type")
